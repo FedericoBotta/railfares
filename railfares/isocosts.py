ebc68ba58945ab7278686538993a03aaee3bbef5
FedericoBotta <federico.botta@wbs.ac.uk>
2022-07-22 17:29:05 +0100

    Improved flow finding using station clusters and return flows

--- railfares/isocosts.py
+++ railfares/isocosts.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 project_dir = '/Users/fb394/Documents/GitHub/railfares/'
 
-starting_station = 'london paddington'
+starting_station = 'exeter st davids'
 
 tickets = data_parsing.get_ticket_type_records(project_dir)
 
 validity = data_parsing.get_ticket_validity(project_dir)
 
-val_code = validity[validity['out_days'] == '01']['validity_code'].to_list()
+# val_code = validity[validity['out_days'] == '01']['validity_code'].to_list()
+
+val_code = validity['validity_code'].to_list()
 
 single_tickets = pd.DataFrame([x for idx, x in tickets.iterrows() if x['end_date'] == '31122999' and x['tkt_class'] == '2' and x['tkt_type'] == 'S' and x['validity_code'] in val_code and 'anytime' in x['description'].lower()])
 
@@ -17,33 +19,121 @@ exeter_nlc = data_parsing.get_station_code_from_name(starting_station, project_d
 
 flow_df = data_parsing.get_flow_records('flow', project_dir)
 
-exeter_flows_df = flow_df[flow_df['origin_code'] == exeter_nlc[0]]
+
+
+stations_codes = data_parsing.get_cluster_from_nlc(exeter_nlc[0], project_dir)['cluster_id'].to_list()
+stations_codes.append(exeter_nlc[0])
+
+
+
+
+exeter_flows_df = flow_df[flow_df['origin_code'].isin(stations_codes)]
+
+
+# exeter_flows_df = flow_df[flow_df['origin_code'] == exeter_nlc[0]]
 
 fares_df = data_parsing.get_flow_records('fares', project_dir)
 
 exeter_list = exeter_flows_df['flow_id'].to_list()
 
-exeter_fares_df = fares_df[fares_df['flow_id'].apply(lambda x: x in exeter_list)]
+exeter_fares_df = fares_df[fares_df['flow_id'].isin(exeter_list)]
 
-exeter_tickets = tickets[tickets['ticket_code'].apply(lambda x: x in exeter_fares_df['ticket_code'].to_list())]
+exeter_tickets = tickets[tickets['ticket_code'].isin(exeter_fares_df['ticket_code'].to_list())]
 
-exeter_singles = exeter_fares_df[exeter_fares_df['ticket_code'].apply(lambda x: x in single_tickets['ticket_code'].to_list())]
+exeter_singles = exeter_fares_df[exeter_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())]
 
 exeter_singles['fare'] = exeter_singles['fare'].astype(int)/100
 
 
 
-budget = 30
+
+
+dest_exeter_flows_df = flow_df[(flow_df['destination_code'].isin(stations_codes)) & (flow_df['direction'] == 'R')]
+
+
+
+# dest_exeter_flows_df = flow_df[(flow_df['destination_code'] == exeter_nlc[0]) & (flow_df['direction'] == 'R')]
+
+dest_exeter_list = dest_exeter_flows_df['flow_id'].to_list()
+
+dest_exeter_fares_df = fares_df[fares_df['flow_id'].isin(dest_exeter_list)]
+
+dest_exeter_tickets = tickets[tickets['ticket_code'].isin(dest_exeter_fares_df['ticket_code'].to_list())]
+
+dest_exeter_singles = dest_exeter_fares_df[dest_exeter_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())]
+
+dest_exeter_singles['fare'] = dest_exeter_singles['fare'].astype(int)/100
+
+
+
+
+
+
+
+
+
+budget = 40
 
 isocost = exeter_singles[exeter_singles['fare'].apply(lambda x: x <= budget)]
 
-isocost_route = exeter_flows_df[(exeter_flows_df['flow_id'].apply(lambda x: x in isocost['flow_id'].to_list()) & exeter_flows_df['end_date'].apply(lambda x: x == '31122999'))]
+temp_isocost_route = exeter_flows_df[(exeter_flows_df['flow_id'].isin(isocost['flow_id'].to_list())) & (exeter_flows_df['end_date'] == '31122999')]
 
-isocost_destinations = data_parsing.get_station_name_from_code(isocost_route['destination_code'], project_dir)
+
+
+temp_isocost_route['bool'] = [data_parsing.get_nlc_from_cluster(x, project_dir).empty for x in temp_isocost_route['destination_code']]
+stations_route = temp_isocost_route[temp_isocost_route['bool'] == True]
+clusters_route = temp_isocost_route[temp_isocost_route['bool'] == False]
+unique_clusters = pd.Series(clusters_route['destination_code'].unique())
+
+disagr_clusters = clusters_route.merge(data_parsing.get_nlc_from_cluster(pd.Series(unique_clusters),project_dir)[['cluster_id', 'cluster_nlc']], left_on = 'destination_code', right_on = 'cluster_id')
+
+isocost_route = pd.concat([stations_route, disagr_clusters])
+
+
+isocost_route['cluster_nlc'].fillna(isocost_route['destination_code'], inplace = True)
+
+
+isocost_destinations = data_parsing.get_station_name_from_code(isocost_route['cluster_nlc'], project_dir)
 
 isocost_fare = isocost_route.merge(isocost[['flow_id','fare']], left_on = 'flow_id', right_on = 'flow_id', how = 'left')
 
-destination_stations = isocost_destinations.merge(isocost_fare, left_on = 'nlc_code', right_on = 'destination_code')
+destination_stations = isocost_destinations.merge(isocost_fare, left_on = 'nlc_code', right_on = 'cluster_nlc')
+
+
+
+dest_isocost = dest_exeter_singles[dest_exeter_singles['fare'].apply(lambda x: x <= budget)]
+
+temp_isocost_route = dest_exeter_flows_df[(dest_exeter_flows_df['flow_id'].isin(dest_isocost['flow_id'].to_list())) & (dest_exeter_flows_df['end_date'] == '31122999')]
+
+
+
+temp_isocost_route['bool'] = [data_parsing.get_nlc_from_cluster(x, project_dir).empty for x in temp_isocost_route['origin_code']]
+stations_route = temp_isocost_route[temp_isocost_route['bool'] == True]
+clusters_route = temp_isocost_route[temp_isocost_route['bool'] == False]
+unique_clusters = pd.Series(clusters_route['origin_code'].unique())
+
+disagr_clusters = clusters_route.merge(data_parsing.get_nlc_from_cluster(pd.Series(unique_clusters),project_dir)[['cluster_id', 'cluster_nlc']], left_on = 'origin_code', right_on = 'cluster_id')
+
+dest_isocost_route = pd.concat([stations_route, disagr_clusters])
+
+
+dest_isocost_route['cluster_nlc'].fillna(dest_isocost_route['origin_code'], inplace = True)
+
+
+
+dest_isocost_destinations = data_parsing.get_station_name_from_code(dest_isocost_route['cluster_nlc'], project_dir)
+
+dest_isocost_fare = dest_isocost_route.merge(dest_isocost[['flow_id','fare']], left_on = 'flow_id', right_on = 'flow_id', how = 'left')
+
+destination_stations_1 = dest_isocost_destinations.merge(dest_isocost_fare, left_on = 'nlc_code', right_on = 'cluster_nlc')
+
+
+
+test = pd.concat([destination_stations, destination_stations_1])
+data_parsing.plot_isocost_stations(data_parsing.get_station_code_from_name(starting_station, project_dir)['crs_code'][0], gpd.GeoDataFrame(test), project_dir + 'test_isocost.html', project_dir)
+
+
+
 
 data_parsing.plot_isocost_stations(data_parsing.get_station_code_from_name(starting_station, project_dir)['crs_code'][0], destination_stations, project_dir + 'isocost.html', project_dir)
 
