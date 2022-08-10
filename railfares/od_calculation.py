388bda9e7ee7cd322471851f9cd86b34a502b970
FedericoBotta <federico.botta@wbs.ac.uk>
2022-08-10 17:45:26 +0100

    Improved station finder in origin-destination calculation

--- railfares/od_calculation.py
+++ railfares/od_calculation.py
@@ -1,5 +1,6 @@
 import railfares.data_parsing as data_parsing
 import pandas as pd
+import json
 
 
 project_dir = '/Users/fb394/Documents/GitHub/railfares/'
@@ -17,37 +18,61 @@ project_dir = '/Users/fb394/Documents/GitHub/railfares/'
 
 
 
-station_gdf = data_parsing.get_station_location(project_dir)
-loc_records_df = data_parsing.get_location_records('location record', project_dir)[['nlc_code', 'crs_code']]
-station_gdf = station_gdf.merge(loc_records_df, left_on = 'CRS Code', right_on = 'crs_code', how = 'left').drop('crs_code',1).drop_duplicates().reset_index(drop = True)
 
 
-stations_nlc_dict = {}
+# station_gdf = data_parsing.get_station_location(project_dir)
 
-for idx, row in station_gdf.iterrows():
+# loc_records_df = data_parsing.get_location_records('location record', project_dir)[['nlc_code', 'crs_code']]
+
+# stations_nlc_dict = {}
+
+# for idx, row in station_gdf.iterrows():
     
-    stations_codes = data_parsing.get_cluster_from_nlc(row['nlc_code'], project_dir)['cluster_id'].to_list()
-    stations_codes.append(row['nlc_code'])
-    stations_nlc_dict[row['Station name']] = stations_codes
-    print(idx)
+#     stations_codes = data_parsing.get_cluster_from_nlc(row['nlc_code'], project_dir)['cluster_id'].to_list()
+#     stations_codes.append(row['nlc_code'])
+#     stations_nlc_dict[row['Station name']] = stations_codes
+#     print(idx)
+
+
+
+
+# data_parsing.get_all_station_nlc_codes(project_dir, print_progress = True, outpath = project_dir + 'all_station_nlc_codes')
+
+
+# the following lines are old codes, replaced by the function called at line 42
+
+# dest = loc_records_df[loc_records_df['nlc_code'].isin(flow_df['destination_code'].to_list())]['nlc_code'].unique()
+# orig = loc_records_df[loc_records_df['nlc_code'].isin(flow_df['origin_code'].to_list())]['nlc_code'].unique()
+# joint = list(orig) + list(set(dest) - set(orig))
+# all_stations_nlcs = loc_records_df[(loc_records_df['nlc_code'].isin(joint)) & (loc_records_df['end_date'] == '31122999')].reset_index()
+
+# stations_nlc_dict = {}
+
+# for idx, row in all_stations_nlcs.iterrows():
+    
+#     stations_codes = data_parsing.get_cluster_from_nlc(row['nlc_code'], project_dir)['cluster_id'].to_list()
+#     stations_codes.append(row['nlc_code'])
+#     stations_nlc_dict[row['description'].rstrip()] = stations_codes
+#     print(idx)
 
 
 
 
 
+with open(project_dir + 'all_station_nlc_codes.json', 'r') as fp:
+    stations_nlc_dict = json.load(fp)
 
+
+
+flow_df, fares_df = data_parsing.get_flow_records('both', project_dir)
 tickets = data_parsing.get_ticket_type_records(project_dir)
 validity = data_parsing.get_ticket_validity(project_dir)
 # val_code = validity[validity['out_days'] == '01']['validity_code'].to_list()
 val_code = validity['validity_code'].to_list()
 single_tickets = pd.DataFrame([x for idx, x in tickets.iterrows() if x['end_date'] == '31122999' and x['tkt_class'] == '2' and x['tkt_type'] == 'S' and x['validity_code'] in val_code and 'anytime' in x['description'].lower()])
-
-
-flow_df, fares_df = data_parsing.get_flow_records('both', project_dir)
-
+loc_records_df = data_parsing.get_location_records('location record', project_dir)[['description', 'nlc_code', 'crs_code']]
 station_gdf = data_parsing.get_station_location(project_dir)
 
-loc_records_df = data_parsing.get_location_records('location record', project_dir)[['nlc_code', 'crs_code']]
 
 clusters_dict = data_parsing.get_cluster_nlc_dict(project_dir)
 
@@ -63,13 +88,15 @@ for key, value in stations_nlc_dict.items():
     # get fares corresponding to the flows
     station_fares_df = fares_df[fares_df['flow_id'].isin(station_list)]
     # select fares corresponding to given ticket type
-    station_singles = station_fares_df[station_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())]
+    station_singles = station_fares_df[station_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())].copy()
     #  convert fare to pounds
     station_singles['fare'] = station_singles['fare'].astype(int)/100
+   
     # subset flows to only those corresponding to given ticket type
-    temp_isocost_route = station_flows_df[(station_flows_df['flow_id'].isin(station_singles['flow_id'].to_list())) & (station_flows_df['end_date'] == '31122999')]
+    temp_isocost_route = station_flows_df[(station_flows_df['flow_id'].isin(station_singles['flow_id'].to_list())) & (station_flows_df['end_date'] == '31122999')].copy()
     # create boolean column to check if destination station is in cluster or not
     temp_isocost_route['bool'] = [x in clusters_dict for x in temp_isocost_route['destination_code']]
+
     # separate stations from clusters
     stations_route = temp_isocost_route[temp_isocost_route['bool'] == False]
     clusters_route = temp_isocost_route[temp_isocost_route['bool'] == True]
@@ -104,12 +131,12 @@ for key, value in stations_nlc_dict.items():
     inverse_station_flows_df = flow_df[(flow_df['destination_code'].isin(value)) & (flow_df['end_date'] == '31122999') & (flow_df['direction'] == 'R')]
     inverse_station_list = inverse_station_flows_df['flow_id'].to_list()
     inverse_station_fares_df = fares_df[fares_df['flow_id'].isin(inverse_station_list)]
-    inverse_station_singles = inverse_station_fares_df[inverse_station_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())]
+    inverse_station_singles = inverse_station_fares_df[inverse_station_fares_df['ticket_code'].isin(single_tickets['ticket_code'].to_list())].copy()
     inverse_station_singles['fare'] = inverse_station_singles['fare'].astype(int)/100
     
     
     
-    temp_isocost_route = inverse_station_flows_df[(inverse_station_flows_df['flow_id'].isin(inverse_station_singles['flow_id'].to_list())) & (inverse_station_flows_df['end_date'] == '31122999')]
+    temp_isocost_route = inverse_station_flows_df[(inverse_station_flows_df['flow_id'].isin(inverse_station_singles['flow_id'].to_list())) & (inverse_station_flows_df['end_date'] == '31122999')].copy()
     
     temp_isocost_route['bool'] = [x in clusters_dict  for x in temp_isocost_route['origin_code']]
     stations_route = temp_isocost_route[temp_isocost_route['bool'] == False]
@@ -129,21 +156,29 @@ for key, value in stations_nlc_dict.items():
         
         station_code = [station_code]
     
-    station_nlc = loc_records_df[loc_records_df['nlc_code'].apply(lambda x: any([str(k) in x for k in station_code]))].drop_duplicates()
+    station_nlc = loc_records_df[loc_records_df['nlc_code'].isin(station_code)].drop_duplicates()
     
     isocost_destinations = station_gdf.merge(station_nlc, left_on = 'CRS Code', right_on = 'crs_code', how = 'inner')
     
     isocost_fare = isocost_route.merge(inverse_station_singles[['flow_id','fare']], left_on = 'flow_id', right_on = 'flow_id', how = 'left')
     inverse_destination_stations = isocost_destinations.merge(isocost_fare, left_on = 'nlc_code', right_on = 'cluster_nlc')
-    # inverse_destination_stations.rename(column = {'Station name': 'Temp station name', 'nlc_code': 'temp nlc_code',
-    #                                               'origin_code': 'temp origin_code', 'destination_code': 'temp destination_code',
-    #                                               'cluster_id': 'temp cluster_id', 'cluster_nlc': 'temp cluster_nlc'}, inplace = True)
+    inverse_destination_stations.rename(columns = {'origin_code': 'temp origin_code', 'destination_code': 'temp destination_code'}, inplace = True)
     
-    # inverse_destination_stations.rename(column = {'Temp station name': })
+    inverse_destination_stations.rename(columns = {'temp origin_code': 'destination_code', 'temp destination_code': 'origin_code'}, inplace = True)
     
-    od_df = pd.concat([destination_stations, inverse_destination_stations])[['Station name', 'geometry', 'nlc_code', 'origin_code','destination_code', 
+    if not destination_stations.empty and not inverse_destination_stations.empty:
+        
+        od_df = pd.concat([destination_stations, inverse_destination_stations], ignore_index = True)[['Station name', 'geometry', 'nlc_code', 'origin_code','destination_code', 
                                                                                                      'route_code', 'end_date', 'start_date', 'toc',
                                                                                                      'flow_id', 'cluster_id', 'fare']]
+    elif inverse_destination_stations.empty:
+        
+        od_df = destination_stations
+    
+    elif destination_stations.empty:
+        
+        od_df = inverse_destination_stations
+        
     od_df.rename(columns = {'Station name': 'Destination station name'}, inplace = True)
     od_df ['Origin station name'] = key
     
@@ -156,9 +191,9 @@ for key, value in stations_nlc_dict.items():
     
     od_list = pd.concat([od_list, od_df_min])
     
-    print(progr)
+    print('Station: ' ,key, ', index ', progr)
     progr = progr + 1
 
-
+od_list.to_csv(project_dir + 'od_minimum_cost_matrix.csv')
 
 
